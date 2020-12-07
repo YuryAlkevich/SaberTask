610755c79704894bc4e5dd18f58a9e46fe6ea9bd
YuryAlkevich <letterdotandnumber@gmail.com>
2020-12-07 10:39:30 +0300

    code reformat

--- tasks/Task3.cpp
+++ tasks/Task3.cpp
@@ -6,15 +6,13 @@
 
 using namespace SaberTask;
 
-struct SerializeNode
-{
+struct SerializeNode {
     int64_t randIdx = -1;
     int64_t dataSize = -1;
 };
 
-void List::serialize(std::FILE *file)
-{
-    if(!file)
+void List::serialize(std::FILE *file) {
+    if (!file)
         return;
 
     std::vector<SerializeNode> nodes(count);
@@ -23,41 +21,37 @@ void List::serialize(std::FILE *file)
 
     // create item address to index association
     auto listPtr = head;
-    for(size_t i = 0; i < count; i++, listPtr = listPtr->next)
-    {
+    for (size_t i = 0; i < count; i++, listPtr = listPtr->next) {
         auto key = reinterpret_cast<intptr_t>(listPtr);
         indexMap[key] = i;
     }
 
     // write elements count first
-    std::fwrite(&count, sizeof(count), 1 , file);
+    std::fwrite(&count, sizeof(count), 1, file);
 
     // fill nodes with indexes instead of item addresses of source list
     // and write them to a file
     listPtr = head;
-    for(size_t i = 0; i < count; i++, listPtr = listPtr->next)
-    {
+    for (size_t i = 0; i < count; i++, listPtr = listPtr->next) {
         nodes[i].dataSize = listPtr->data.size();
 
         auto randKey = reinterpret_cast<intptr_t>(listPtr->rand);
         nodes[i].randIdx = indexMap[randKey];
 
-        std::fwrite(&nodes[i],sizeof(SerializeNode), 1, file);
+        std::fwrite(&nodes[i], sizeof(SerializeNode), 1, file);
         std::fwrite(listPtr->data.c_str(), listPtr->data.size(), 1, file);
     }
 }
 
-void List::deserialize(std::FILE *file)
-{
-    if(!file)
+void List::deserialize(std::FILE *file) {
+    if (!file)
         return;
 
     std::fread(&count, sizeof(count), 1, file);
 
-    auto* nodes = new ListNode[count];
+    auto *nodes = new ListNode[count];
     SerializeNode serializedNode;
-    for(int i = 0; i < count; i++)
-    {
+    for (int i = 0; i < count; i++) {
         std::fread(&serializedNode, sizeof(SerializeNode), 1, file);
         std::vector<char> buffer(serializedNode.dataSize);
         std::fread(buffer.data(), buffer.size(), 1, file);
@@ -72,19 +66,16 @@ void List::deserialize(std::FILE *file)
     tail = nodes + count - 1;
 }
 
-ListNode& List::pushFront(const std::string &data) {
+ListNode &List::pushFront(const std::string &data) {
     auto it = new ListNode();
     it->data = data;
     it->prev = nullptr;
     it->next = head;
 
-    if(!head)
-    {
+    if (!head) {
         head = it;
         tail = it;
-    }
-    else
-    {
+    } else {
         head->prev = it;
         head = it;
     }
@@ -94,20 +85,16 @@ ListNode& List::pushFront(const std::string &data) {
     return *it;
 }
 
-ListNode& List::pushBack(const std::string &data)
-{
+ListNode &List::pushBack(const std::string &data) {
     auto it = new ListNode();
     it->data = data;
     it->prev = tail;
     it->next = nullptr;
 
-    if(!tail)
-    {
+    if (!tail) {
         tail = it;
         head = it;
-    }
-    else
-    {
+    } else {
         tail->next = it;
         tail = it;
     }
@@ -117,33 +104,30 @@ ListNode& List::pushBack(const std::string &data)
     return *it;
 }
 
-ListNode& List::find(const std::string &data) {
-    return *std::find_if(this->begin(), this->end(), [&](const ListNode& node)
-    {
+ListNode &List::find(const std::string &data) {
+    return *std::find_if(this->begin(), this->end(), [&](const ListNode &node) {
         return data == node.data;
     });
 }
 
 List::iterator List::begin() {
-    return { this->head };
+    return {this->head};
 }
 
 List::iterator List::end() {
-    return {nullptr };
+    return {nullptr};
 }
 
-List::ListIterator::ListIterator(ListNode *node)
-{
+List::ListIterator::ListIterator(ListNode *node) {
     _it = node;
 }
 
-List::ListIterator& List::ListIterator::operator++()
-{
+List::ListIterator &List::ListIterator::operator++() {
     _it = _it->next;
     return *this;
 }
 
-ListNode& List::ListIterator::operator*() {
+ListNode &List::ListIterator::operator*() {
     return *_it;
 }
 
@@ -153,7 +137,7 @@ List::ListIterator List::ListIterator::operator++(int) {
     return retval;
 }
 
-bool List::ListIterator::operator==(const ListIterator& other) {
+bool List::ListIterator::operator==(const ListIterator &other) {
     return _it == other._it;
 }
 
